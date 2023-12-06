9c690f37e541f0e2566d4e57390c99a3213d7156
Rui Xu <rae@dhcp-128-189-225-50.ubcsecure.wireless.ubc.ca>
2023-12-06 00:23:20 -0800

    combined bar plots from DeSeq2 analysis

--- DeSeq2_Cowplot.R
+++ DeSeq2_Cowplot.R
@@ -84,3 +84,97 @@ ggsave('combined_volcano_with_legend.png',
        height = 10,
        combined_volcano_w_legend)
 
+
+##### DeSeq2 Bar Plots #####
+
+# To get table of results of depression
+sigASVs_depression <- res_depression %>% 
+  filter(padj<0.01 & abs(log2FoldChange)>2) %>%
+  dplyr::rename(ASV=row)
+View(sigASVs_depression)
+# Get only asv names
+sigASVs_vec_depression <- sigASVs_depression %>%
+  pull(ASV)
+
+# Prune Depression phyloseq file #
+depression_DESeq <- prune_taxa(sigASVs_vec_depression,parkinsons_final_depression)
+sigASVs_depression <- tax_table(depression_DESeq) %>% as.data.frame() %>%
+  rownames_to_column(var="ASV") %>%
+  right_join(sigASVs_depression) %>%
+  arrange(log2FoldChange) %>%
+  mutate(Genus = make.unique(Genus)) %>%
+  mutate(Genus = factor(Genus, levels=unique(Genus)))
+
+# generate depression bar plot #
+depression_bar_plot <- ggplot(sigASVs_depression) +
+  geom_bar(aes(x=Genus, y=log2FoldChange), stat="identity")+
+  ggtitle('Depression DeSeq2 Bar Plot') +
+  geom_errorbar(aes(x=Genus, ymin=log2FoldChange-lfcSE, ymax=log2FoldChange+lfcSE)) +
+  theme(plot.title = element_text(hjust = 0.5))+
+  theme(axis.text.x = element_text(angle=90, hjust=1, vjust=0.5))
+depression_bar_plot
+
+# To get table of results of anxiety
+sigASVs_anxiety <- res_anxiety %>% 
+  filter(padj<0.01 & abs(log2FoldChange)>2) %>%
+  dplyr::rename(ASV=row)
+View(sigASVs_anxiety)
+# Get only asv names
+sigASVs_vec_anxiety <- sigASVs_anxiety %>%
+  pull(ASV)
+
+# Prune Anxiety phyloseq file #
+anxiety_DESeq <- prune_taxa(sigASVs_vec_anxiety,parkinsons_final_anxiety)
+sigASVs_anxiety <- tax_table(anxiety_DESeq) %>% as.data.frame() %>%
+  rownames_to_column(var="ASV") %>%
+  right_join(sigASVs_anxiety) %>%
+  arrange(log2FoldChange) %>%
+  mutate(Genus = make.unique(Genus)) %>%
+  mutate(Genus = factor(Genus, levels=unique(Genus)))
+
+# generate anxiety bar plot #
+anxiety_bar_plot <- ggplot(sigASVs_anxiety) +
+  geom_bar(aes(x=Genus, y=log2FoldChange), stat="identity")+
+  ggtitle('Anxiety DeSeq2 Bar Plot') +
+  geom_errorbar(aes(x=Genus, ymin=log2FoldChange-lfcSE, ymax=log2FoldChange+lfcSE)) +
+  theme(plot.title = element_text(hjust = 0.5))+
+  theme(axis.text.x = element_text(angle=90, hjust=1, vjust=0.5))
+anxiety_bar_plot
+
+# To get table of results of sleep problems
+sigASVs_sleep <- res_sleep %>% 
+  filter(padj<0.01 & abs(log2FoldChange)>2) %>%
+  dplyr::rename(ASV=row)
+View(sigASVs_sleep)
+# Get only asv names
+sigASVs_vec_sleep <- sigASVs_sleep %>%
+  pull(ASV)
+
+# Prune Sleep Problem phyloseq file #
+sleep_problem_DESeq <- prune_taxa(sigASVs_vec_sleep,parkinsons_final_sleep)
+sigASVs_sleep <- tax_table(sleep_problem_DESeq) %>% as.data.frame() %>%
+  rownames_to_column(var="ASV") %>%
+  right_join(sigASVs_sleep) %>%
+  arrange(log2FoldChange) %>%
+  mutate(Genus = make.unique(Genus)) %>%
+  mutate(Genus = factor(Genus, levels=unique(Genus)))
+
+# generate anxiety bar plot #
+sleep_problem_bar_plot <- ggplot(sigASVs_sleep) +
+  geom_bar(aes(x=Genus, y=log2FoldChange), stat="identity")+
+  ggtitle('Sleep Problem DeSeq2 Bar Plot') +
+  geom_errorbar(aes(x=Genus, ymin=log2FoldChange-lfcSE, ymax=log2FoldChange+lfcSE)) +
+  theme(plot.title = element_text(hjust = 0.5))+
+  theme(axis.text.x = element_text(angle=90, hjust=1, vjust=0.5))
+sleep_problem_bar_plot
+
+combined_bar_plot_deseq <- plot_grid(depression_bar_plot, 
+                                     anxiety_bar_plot, 
+                              sleep_problem_bar_plot,
+                              labels = c('A', 'B','C'), nrow = 1)
+combined_bar_plot_deseq
+
+ggsave('combined_bar_plot.png',
+       width = 20,
+       height = 10,
+       combined_bar_plot_deseq)
